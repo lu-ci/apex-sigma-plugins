63f497944e051a913c40200f03d03ae492cf213a
AXAz0r <thealeksaradovic@gmail.com>
2017-06-30 13:32:01 +0200

    Fix Permission Data In Modules

--- development/test.py
+++ development/test.py
@@ -3,4 +3,4 @@ import yaml
 async def test(cmd, message, args):
     with open(cmd.resource('test.yml')) as test_file:
         data = yaml.safe_load(test_file)
-    await message.channel.send('OK.')
+    await message.channel.send('Ok.')
