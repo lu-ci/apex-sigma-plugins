99327bd5e76c98dc6eca5dc1da32702f69e233b6
AXAz0r <thealeksaradovic@gmail.com>
2017-06-30 18:37:45 +0200

    Minify Test

--- development/test.py
+++ development/test.py
@@ -1,6 +1,2 @@
-import yaml
-
 async def test(cmd, message, args):
-    with open(cmd.resource('test.yml')) as test_file:
-        data = yaml.safe_load(test_file)
     await message.channel.send('Ok.')
