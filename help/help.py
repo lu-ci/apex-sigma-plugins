a253bc14d80811b127d1596439dce69929f833d9
AXAz0r <thealeksaradovic@gmail.com>
2017-07-01 16:19:02 +0200

    Fixed Bad Command Usage In Module YAMLs

---
+++ help/help.py
@@ -0,0 +1,29 @@
+import discord
+
+
+async def help(cmd, message, args):
+    if args:
+        cmd_name = ''.join(args).lower()
+        if cmd_name in cmd.bot.modules.alts:
+            cmd_name = cmd.bot.modules.alts[cmd]
+        if cmd_name in cmd.bot.modules.commands:
+            command = cmd.bot.modules.commands[cmd_name]
+            response = discord.Embed(color=0x1B6F5F, title=f'📄 {command.name.upper()} Help and Information')
+            response.add_field(name='Usage Example', value=f'`{command.usage}`', inline=False)
+            response.add_field(name='Command Description', value=f'```\n{command.desc}\n```', inline=False)
+        else:
+            response = discord.Embed(color=0x696969, title='🔍 No such command was found...')
+    else:
+        aurora_image = 'https://i.imgur.com/IsUF5x8.png'
+        sigma_image = 'https://i.imgur.com/mGyqMe1.png'
+        sigma_color = 0x1B6F5F
+        sigma_title = 'Apex Sigma: The Database Giant'
+        sigma_url = cmd.bot.cfg.pref.website
+        command_url = sigma_url + '/commands'
+        response = discord.Embed(color=sigma_color)
+        response.set_author(name=sigma_title, url=sigma_url, icon_url=sigma_image)
+        response.set_thumbnail(url=sigma_image)
+        response.add_field(name='Website', value=f'[Link]({sigma_url})', inline=True)
+        response.add_field(name='Commands', value=f'[Link]({command_url})', inline=True)
+        response.set_footer(text='© by the Aurora Project under GPLv3.', icon_url=aurora_image)
+    await message.channel.send(embed=response)
