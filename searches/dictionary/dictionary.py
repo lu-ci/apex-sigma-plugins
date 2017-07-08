2b23846d9d760a56b3502e9c5c864a330c8f5e97
AXAz0r <thealeksaradovic@gmail.com>
2017-07-08 14:09:34 +0200

    Oxford Dictionary

---
+++ searches/dictionary/dictionary.py
@@ -0,0 +1,38 @@
+import json
+import aiohttp
+import discord
+
+
+async def dictionary(cmd, message, args):
+    if 'app_id' in cmd.cfg and 'app_key' in cmd.cfg:
+        headers = {
+            'Accept': 'application/json',
+            'app_id': cmd.cfg['app_id'],
+            'app_key': cmd.cfg['app_key']
+        }
+        if args:
+            qry = ' '.join(args)
+            api_url = f'https://od-api.oxforddictionaries.com/api/v1/entries/en/{qry}'
+            async with aiohttp.ClientSession() as session:
+                async with session.get(api_url, headers=headers) as data_response:
+                    data = await data_response.read()
+                    try:
+                        data = json.loads(data)
+                    except json.JSONDecodeError:
+                        data = {'results': []}
+            if data['results']:
+                data = data['results'][0]['lexicalEntries'][0]['entries'][0]
+                etymology = data['etymologies'][0]
+                senses = []
+                for sense in data['senses']:
+                    senses.append(f'{sense["domains"][0]}: {sense["definitions"][0]}')
+                response = discord.Embed(color=0x0099FF, title=f'📘 Oxford Dictionary: `{qry}`')
+                response.add_field(name='Etymology', value=etymology, inline=False)
+                response.add_field(name='Senses', value='\n'.join(senses[:10]), inline=False)
+            else:
+                response = discord.Embed(color=0x696969, title='🔍 No results.')
+        else:
+            response = discord.Embed(color=0xDB0000, title='❗ Nothing inputted.')
+    else:
+        response = discord.Embed(color=0xDB0000, title='❗ The API Key is missing.')
+    await message.channel.send(embed=response)
