ba0a96abb1963930073b65e99833918a145ce70e
AXAz0r <thealeksaradovic@gmail.com>
2017-07-16 00:15:54 +0200

    Log Unmute

--- moderation/messages/textunmute.py
+++ moderation/messages/textunmute.py
@@ -1,6 +1,22 @@
-﻿import discord
+﻿import arrow
+import discord
 from sigma.core.utilities.permission_processing import hierarchy_permit
+from sigma.core.utilities.server_bound_logging import log_event
+from sigma.core.utilities.data_processing import user_avatar
 
+
+def generate_log_embed(message, target, args):
+    log_embed = discord.Embed(color=0x696969, timestamp=arrow.utcnow().datetime)
+    log_embed.set_author(name='A Member Has Been Unmuted', icon_url=user_avatar(target))
+    log_embed.add_field(name='🔊 Unmuted User',
+                        value=f'{target.mention}\n{target.name}#{target.discriminator}', inline=True)
+    author = message.author
+    log_embed.add_field(name='🛡 Responsible',
+                        value=f'{author.mention}\n{author.name}#{author.discriminator}', inline=True)
+    if len(args) > 1:
+        log_embed.add_field(name='📄 Reason', value=f"```\n{' '.join(args[1:])}\n```", inline=False)
+    log_embed.set_footer(text=f'UserID: {target.id}')
+    return log_embed
 
 async def textunmute(cmd, message, args):
     if not message.author.permissions_in(message.channel).manage_messages:
@@ -29,4 +45,6 @@ async def textunmute(cmd, message, args):
                         mute_list.remove(target.id)
                         cmd.db.set_guild_settings(message.guild.id, 'MutedUsers', mute_list)
                         response = discord.Embed(color=0x77B255, title=f'✅ {target.display_name} has been unmuted.')
+                        log_embed = generate_log_embed(message, target, args)
+                        await log_event(cmd.db, message.guild, log_embed)
     await message.channel.send(embed=response)
