f5c8076fb342ba528a17c721ad2c8d4782456537
AXAz0r <thealeksaradovic@gmail.com>
2017-07-16 03:21:08 +0200

    Wallet, XP and Profile Stats Commands

--- core_functions/currency/currency_message.py
+++ core_functions/currency/currency_message.py
@@ -8,5 +8,5 @@ async def currency_message(ev, message):
             if not message.content.startswith(prefix):
                 if not ev.bot.cooldown.on_cooldown(ev.name, message.author):
                     points = secrets.randbelow(3)
-                    ev.db.add_experience(message.author, message.guild, points)
+                    ev.db.add_currency(message.author, message.guild, points)
                     ev.bot.cooldown.set_cooldown(ev.name, message.author, 60)
