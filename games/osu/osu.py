4dec7f86b831d25adfacb5ecdb0374c2dd2055f4
AXAz0r <thealeksaradovic@gmail.com>
2017-07-16 13:32:08 +0200

    Added the rest of Game Commads

---
+++ games/osu/osu.py
@@ -0,0 +1,27 @@
+﻿import discord
+import aiohttp
+import lxml.html as l
+
+osu_logo = 'http://w.ppy.sh/c/c9/Logo.png'
+
+
+async def osu(cmd, message, args):
+    if args:
+        osu_input = ' '.join(args)
+        try:
+            profile_url = 'https://osu.ppy.sh/u/' + osu_input
+            async with aiohttp.ClientSession() as session:
+                async with session.get(profile_url) as data:
+                    page = await data.text()
+            root = l.fromstring(page)
+            username = root.cssselect('.profile-username')[0].text[:-1]
+            user_color = str(message.author.color)[1:]
+            sig_url = 'https://lemmmy.pw/osusig/sig.php?colour=hex' + user_color + '&uname=' + osu_input
+            response = discord.Embed(color=message.author.color)
+            response.set_image(url=sig_url)
+            response.set_author(name=username + '\'s osu! Profile', url=profile_url, icon_url=osu_logo)
+        except Exception:
+            response = discord.Embed(color=0xDB0000, title='❗ Unable to retrieve profile.')
+    else:
+        response = discord.Embed(color=0xDB0000, title='❗ Nothing inputted.')
+    await message.channel.send(None, embed=response)
---
+++ games/pokemon/pokemon.py
@@ -0,0 +1,57 @@
+﻿import aiohttp
+import discord
+
+
+async def pokemon(cmd, message, args):
+    poke_input = ' '.join(args)
+
+    pokemon_url = ('http://pokeapi.co/api/v2/pokemon/' + poke_input.lower())
+    async with aiohttp.ClientSession() as session:
+        async with session.get(pokemon_url) as data:
+            poke = await data.json()
+    poke_id = str(poke['id'])
+    name = str(poke['name']).title()
+    number = str(poke['order'])
+    embed = discord.Embed(color=0x1ABC9C)
+    height = str(poke['height'] / 10) + 'm'
+    weight = str(poke['weight'] / 10) + 'kg'
+    image = 'https://randompokemon.com/sprites/animated/' + poke_id + '.gif'
+    sprite = poke['sprites']['front_default']
+    embed.set_author(name='#' + number + ': ' + name, icon_url=sprite, url=sprite)
+    embed.set_image(url=image)
+    embed.add_field(name='Measurements', value='```\nHeight: ' + height + '\nWeight: ' + weight + '\n```')
+    type_text = ''
+    type_urls = []
+    ability_text = ''
+    for ptype in poke['types']:
+        type_text += '\n' + ptype['type']['name'].title()
+        type_urls.append(ptype['type']['url'])
+    for ability in poke['abilities']:
+        if ability['is_hidden']:
+            hidden = 'Hidden'
+        else:
+            hidden = 'Visible'
+        ability_text += '\n' + ability['ability']['name'].title() + '\n - ' + hidden
+    weak_against = []
+    strong_against = []
+    good_relations = ['no_damage_from', 'half_damage_from', 'double_damage_to']
+    bad_relations = ['no_damage_to', 'half_damage_to', 'double_damage_from']
+    for type_url in type_urls:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(type_url) as data:
+                type_data = await data.json()
+        dr = type_data['damage_relations']
+        for relation in good_relations:
+            for ptype in dr[relation]:
+                if ptype['name'].title() not in strong_against:
+                    strong_against.append(ptype['name'].title())
+        for relation in bad_relations:
+            for ptype in dr[relation]:
+                if ptype['name'].title() not in weak_against:
+                    weak_against.append(ptype['name'].title())
+    embed.add_field(name='Types', value='```\n' + type_text + '\n```')
+    embed.add_field(name='Abilities', value='```\n' + ability_text + '\n```')
+    embed.add_field(name='Strong Against', value='```\n' + '\n'.join(strong_against) + '\n```')
+    embed.add_field(name='Weak Against', value='```\n' + '\n'.join(weak_against) + '\n```')
+
+    await message.channel.send(None, embed=embed)
