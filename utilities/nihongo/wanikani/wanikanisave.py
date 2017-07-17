a5d95be27cd8562a28807df886b6d33ceb598574
AXAz0r <thealeksaradovic@gmail.com>
2017-07-17 13:08:05 +0200

    Added WaniKani Commands

---
+++ utilities/nihongo/wanikani/wanikanisave.py
@@ -0,0 +1,22 @@
+import discord
+
+
+async def wanikanisave(cmd, message, args):
+    try:
+        await message.delete()
+    except discord.ClientException:
+        pass
+    if args:
+        api_key = ''.join(args)
+        api_document = cmd.db[cmd.db.db_cfg.database]['WaniKani'].find_one({'UserID': message.author.id})
+        data = {'UserID': message.author.id, 'Key': api_key}
+        if api_document:
+            ender = 'updated'
+            cmd.db[cmd.db.db_cfg.database]['WaniKani'].update_one({'UserID': message.author.id}, {'$set': data})
+        else:
+            ender = 'saved'
+            cmd.db[cmd.db.db_cfg.database]['WaniKani'].insert_one(data)
+        response = discord.Embed(color=0x66CC66, title=f'🔑 Your key has been {ender}.')
+    else:
+        response = discord.Embed(color=0xDB0000, title='❗ Nothing inputted.')
+    await message.channel.send(embed=response)
