d76ad0a9d0a82a27251d53e8a3f4e89cb6867f00
AXAz0r <thealeksaradovic@gmail.com>
2017-07-18 22:17:38 +0200

    Table Unflipper

---
+++ fun/unflip/table_unflipper.py
@@ -0,0 +1,26 @@
+import secrets
+from sigma.core.utilities.stats_processing import add_special_stats
+
+async def table_unflipper(ev, message):
+    if '(╯°□°）╯︵ ┻━┻' in message.content:
+        unflip = True
+        if message.guild:
+            flip_settings = ev.db.get_guild_settings(message.guild.id, 'Unflip')
+            if flip_settings is None:
+                unflip = False
+            else:
+                unflip = flip_settings
+        if unflip:
+            add_special_stats(ev.db, 'tables_fixed')
+            table = ['┬─┬ ノ( ^_^ノ)',
+                     '┬─┬ ﾉ(° -°ﾉ)',
+                     '┬─┬ ノ(゜-゜ノ)',
+                     '┬─┬ ノ(ಠ\_ಠノ)',
+                     '┻━┻~~~~  ╯(°□° ╯)',
+                     '┻━┻====  ╯(°□° ╯)',
+                     '┣ﾍ(^▽^ﾍ)Ξ(ﾟ▽ﾟ*)ﾉ┳━┳ There we go~♪',
+                     ' ┬──┬﻿ ¯\_(ツ)',
+                     '(ヘ･_･)ヘ┳━┳',
+                     'ヘ(´° □°)ヘ┳━┳',
+                     '┣ﾍ(≧∇≦ﾍ)… (≧∇≦)/┳━┳']
+            await message.channel.send(secrets.choice(table))
