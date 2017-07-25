1845aa968aabdfafdb075602c799881eaabf3edd
AXAz0r <thealeksaradovic@gmail.com>
2017-07-26 00:44:38 +0200

    Impeersonate Non-Locking

--- utilities/mathematics/impersonate.py
+++ utilities/mathematics/impersonate.py
@@ -1,35 +1,41 @@
 from sigma.core.utilities.data_processing import user_avatar
+from concurrent.futures import ThreadPoolExecutor
 import markovify
+import functools
+import asyncio
 import discord
 import ftfy
 
+threads = ThreadPoolExecutor(max_workers=2)
+loop = asyncio.get_event_loop()
+
 
 async def impersonate(cmd, message, args):
-        if message.mentions:
-            target = message.mentions[0]
+    if message.mentions:
+        target = message.mentions[0]
+    else:
+        if args:
+            target = discord.utils.find(lambda x: x.name.lower() == ' '.join(args).lower(), message.guild.members)
         else:
-            if args:
-                target = discord.utils.find(lambda x: x.name.lower() == ' '.join(args).lower(), message.guild.members)
+            target = message.author
+    if target:
+        chain_data = cmd.db[cmd.db.db_cfg.database]['MarkovChains'].find_one({'UserID': target.id})
+        if chain_data:
+            total_string = ' '.join(chain_data['Chain'])
+            total_string = ftfy.fix_text(total_string)
+            chain = await loop.run_in_executor(threads, functools.partial(markovify.Text, total_string))
+            sentence = await loop.run_in_executor(threads, functools.partial(chain.make_sentence, tries=100))
+            if not sentence:
+                response = discord.Embed(color=0xDB0000, title='😖 I Couldn\'t think of anything...')
             else:
-                target = message.author
-        if target:
-            chain_data = cmd.db[cmd.db.db_cfg.database]['MarkovChains'].find_one({'UserID': target.id})
-            if chain_data:
-                total_string = ' '.join(chain_data['Chain'])
-                total_string = ftfy.fix_text(total_string)
-                chain = markovify.Text(total_string)
-                sentence = chain.make_sentence(tries=100)
-                if not sentence:
-                    response = discord.Embed(color=0xDB0000, title='😖 I Couldn\'t think of anything...')
-                else:
-                    sentence = ftfy.fix_text(sentence)
-                    response = discord.Embed(color=0xbdddf4)
-                    response.set_author(name=target.name, icon_url=user_avatar(target))
-                    response.add_field(name='💭 Hmm... something like...', value=sentence)
-            else:
-                response = discord.Embed(color=0x696969)
-                prefix = cmd.bot.get_prefix(message)
-                title = f'🔍 Chain Data Not Found For {target.name}'
-                value = f'You can make one with `{prefix}collectchain @{target.name} #channel`!'
-                response.add_field(name=title, value=value)
-            await message.channel.send(None, embed=response)
+                sentence = ftfy.fix_text(sentence)
+                response = discord.Embed(color=0xbdddf4)
+                response.set_author(name=target.name, icon_url=user_avatar(target))
+                response.add_field(name='💭 Hmm... something like...', value=sentence)
+        else:
+            response = discord.Embed(color=0x696969)
+            prefix = cmd.bot.get_prefix(message)
+            title = f'🔍 Chain Data Not Found For {target.name}'
+            value = f'You can make one with `{prefix}collectchain @{target.name} #channel`!'
+            response.add_field(name=title, value=value)
+        await message.channel.send(None, embed=response)
