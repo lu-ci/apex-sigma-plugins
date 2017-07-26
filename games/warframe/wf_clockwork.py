5fc981f7e58e2b4207f6932e504ff768ba9f2f58
AXAz0r <thealeksaradovic@gmail.com>
2017-07-26 20:34:26 +0200

    WF Clockwork Change

--- games/warframe/wf_clockwork.py
+++ games/warframe/wf_clockwork.py
@@ -8,57 +8,60 @@ from .nodes.alert_functions import get_alert_data, generate_alert_embed
 async def wf_clockwork(ev):
     while True:
         try:
-            all_guilds = ev.bot.guilds
-            sorties = await get_sortie_data(ev.db)
-            if sorties:
-                sortie_response = generate_sortie_embed(sorties)
-                for guild in all_guilds:
-                    sortie_channel = ev.db.get_guild_settings(guild.id, 'WarframeSortieChannel')
-                    if sortie_channel:
-                        sortie_target_channel = discord.utils.find(lambda x: x.id == sortie_channel, guild.channels)
-                        if sortie_target_channel:
-                            try:
-                                await sortie_target_channel.send(embed=sortie_response)
-                            except discord.ClientException:
-                                pass
-            fissures = await get_fissure_data(ev.db)
-            if fissures:
-                fissure_response = generate_fissure_embed(fissures)
-                for guild in all_guilds:
-                    fissure_channel = ev.db.get_guild_settings(guild.id, 'WarframeFissureChannel')
-                    if fissure_channel:
-                        fissure_target_channel = discord.utils.find(lambda x: x.id == fissure_channel, guild.channels)
-                        try:
-                            await fissure_target_channel.send(embed=fissure_response)
-                        except discord.ClientException:
-                            pass
-            alerts, triggers = await get_alert_data(ev.db)
-            if alerts:
-                alert_response = await generate_alert_embed(alerts)
-                for guild in all_guilds:
-                    mentions = []
-                    if triggers:
-                        for trigger in triggers:
-                            wf_tags = ev.db.get_guild_settings(guild.id, 'WarframeTags')
-                            if wf_tags is None:
-                                wf_tags = {}
-                            if wf_tags:
-                                if trigger in wf_tags:
-                                    bound_role = discord.utils.find(lambda x: x.id == wf_tags[trigger], guild.roles)
-                                    if bound_role:
-                                        mentions.append(bound_role.mention)
-                    alert_channel = ev.db.get_guild_settings(guild.id, 'WarframeAlertChannel')
-                    if alert_channel:
-                        alert_target_channel = discord.utils.find(lambda x: x.id == alert_channel, guild.channels)
-                        try:
-                            if mentions:
-                                await alert_target_channel.send(' '.join(mentions), embed=alert_response)
-                            else:
-                                await alert_target_channel.send(embed=alert_response)
-                        except discord.ClientException:
-                            pass
-        except discord.ClientException:
-            pass
+            await cycle_function(ev)
         except Exception:
             pass
         await asyncio.sleep(5)
+
+async def cycle_function(ev):
+    all_guilds = ev.bot.guilds
+    sorties = await get_sortie_data(ev.db)
+    if sorties:
+        sortie_response = generate_sortie_embed(sorties)
+        for guild in all_guilds:
+            sortie_channel = ev.db.get_guild_settings(guild.id, 'WarframeSortieChannel')
+            if sortie_channel:
+                sortie_target_channel = discord.utils.find(lambda x: x.id == sortie_channel, guild.channels)
+                if sortie_target_channel:
+                    try:
+                        await sortie_target_channel.send(embed=sortie_response)
+                    except Exception:
+                        pass
+    fissures = await get_fissure_data(ev.db)
+    if fissures:
+        fissure_response = generate_fissure_embed(fissures)
+        for guild in all_guilds:
+            fissure_channel = ev.db.get_guild_settings(guild.id, 'WarframeFissureChannel')
+            if fissure_channel:
+                fissure_target_channel = discord.utils.find(lambda x: x.id == fissure_channel, guild.channels)
+                if fissure_target_channel:
+                    try:
+                        await fissure_target_channel.send(embed=fissure_response)
+                    except Exception:
+                        pass
+    alerts, triggers = await get_alert_data(ev.db)
+    if alerts:
+        alert_response = await generate_alert_embed(alerts)
+        for guild in all_guilds:
+            mentions = []
+            if triggers:
+                for trigger in triggers:
+                    wf_tags = ev.db.get_guild_settings(guild.id, 'WarframeTags')
+                    if wf_tags is None:
+                        wf_tags = {}
+                    if wf_tags:
+                        if trigger in wf_tags:
+                            bound_role = discord.utils.find(lambda x: x.id == wf_tags[trigger], guild.roles)
+                            if bound_role:
+                                mentions.append(bound_role.mention)
+            alert_channel = ev.db.get_guild_settings(guild.id, 'WarframeAlertChannel')
+            if alert_channel:
+                alert_target_channel = discord.utils.find(lambda x: x.id == alert_channel, guild.channels)
+                if alert_target_channel:
+                    try:
+                        if mentions:
+                            await alert_target_channel.send(' '.join(mentions), embed=alert_response)
+                        else:
+                            await alert_target_channel.send(embed=alert_response)
+                    except Exception:
+                        pass
