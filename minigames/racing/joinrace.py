19751b2fc6f3ac48d38bedae79ed45cce3f5de06
AXAz0r <thealeksaradovic@gmail.com>
2017-07-27 05:29:35 +0200

    Race Join Fix

--- minigames/racing/joinrace.py
+++ minigames/racing/joinrace.py
@@ -23,13 +23,20 @@ async def joinrace(cmd, message, args):
         if valid_kud:
             race = races[message.channel.id]
             if len(race['users']) < 10:
-                icon = add_participant(message.channel.id, message.author)
-                join_title = f'{icon} {message.author.display_name} joined as a {names[icon]}!'
-                if bet_amt:
-                    cmd.db.rmv_currency(message.author, message.guild, bet_amt)
-                    join_title += f' And bet {bet_amt} {currency}!'
-                    add_to_pool(message.channel.id, bet_amt)
-                response = discord.Embed(color=colors[icon], title=join_title)
+                user_found = False
+                for user in race['users']:
+                    if user['user'].id == message.author:
+                        user_found = True
+                if not user_found:
+                    icon = add_participant(message.channel.id, message.author)
+                    join_title = f'{icon} {message.author.display_name} joined as a {names[icon]}!'
+                    if bet_amt:
+                        cmd.db.rmv_currency(message.author, message.guild, bet_amt)
+                        join_title += f' And bet {bet_amt} {currency}!'
+                        add_to_pool(message.channel.id, bet_amt)
+                    response = discord.Embed(color=colors[icon], title=join_title)
+                else:
+                    response = discord.Embed(color=0xDB0000, title='❗ You already in the race!')
             else:
                 response = discord.Embed(color=0xDB0000, title='❗ Sorry, no more room left!')
         else:
