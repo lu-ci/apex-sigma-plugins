88b279b1ce1ebcc354c723e457488414f09abe7f
AXAz0r <thealeksaradovic@gmail.com>
2017-08-08 14:28:55 +0200

    Add Softban Logging

--- moderation/punishments/softban.py
+++ moderation/punishments/softban.py
@@ -1,6 +1,21 @@
-﻿import discord
+﻿import arrow
+import discord
 from sigma.core.utilities.permission_processing import hierarchy_permit
 from sigma.core.utilities.data_processing import user_avatar
+from sigma.core.utilities.server_bound_logging import log_event
+
+
+def generate_log_embed(message, target, reason):
+    log_response = discord.Embed(color=0x696969, timestamp=arrow.utcnow().datetime)
+    log_response.set_author(name=f'A User Has Been Soft-Banned', icon_url=user_avatar(target))
+    log_response.add_field(name='🔩 Soft-Banned User',
+                           value=f'{target.mention}\n{target.name}#{target.discriminator}', inline=True)
+    author = message.author
+    log_response.add_field(name='🛡 Responsible',
+                           value=f'{author.mention}\n{author.name}#{author.discriminator}', inline=True)
+    log_response.add_field(name='📄 Reason', value=f"```\n{reason}\n```", inline=False)
+    log_response.set_footer(text=f'UserID: {target.id}')
+    return log_response
 
 
 async def softban(cmd, message, args):
@@ -30,6 +45,8 @@ async def softban(cmd, message, args):
                                 pass
                             await target.ban(reason=f'By {message.author.name}: {reason} (Soft)')
                             await target.unban()
+                            log_embed = generate_log_embed(message, target, reason)
+                            await log_event(cmd.db, message.guild, log_embed)
                         else:
                             response = discord.Embed(title='⛔ Can\'t ban above my highest role.', color=0xBE1931)
                     else:
