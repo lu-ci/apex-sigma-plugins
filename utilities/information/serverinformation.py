a3b794e403197c7c73744d3a1fc954c5dbfd91ca
AXAz0r <thealeksaradovic@gmail.com>
2017-08-09 08:23:13 +0200

    Server Info Revised

--- utilities/information/serverinformation.py
+++ utilities/information/serverinformation.py
@@ -1,31 +1,43 @@
+import arrow
 import discord
 
 
 async def serverinformation(cmd, message, args):
-    out_list = []
-    if message.guild:
-        bot_count = 0
-        user_count = 0
-        serv = message.guild
-        for user in serv.members:
-            if user.bot:
-                bot_count += 1
-            else:
-                user_count += 1
-        out_list.append(['Name', serv.name])
-        out_list.append(['Server ID', serv.id])
-        out_list.append(['Created', serv.created_at])
-        out_list.append(['Member Count', str(user_count) + ' (+' + str(bot_count) + ' bots)'])
-        out_list.append(['Owner', serv.owner])
-        out_list.append(['Owner ID', serv.owner_id])
-        out_list.append(['Region', serv.region])
-        out_list.append(['Verification Level', serv.verification_level])
-        out_list.append(['MFA Level', serv.mfa_level])
-        if serv.afk_channel:
-            out_list.append(['AFK Channel', serv.afk_channel])
-            out_list.append(['AFK Timeout', serv.afk_timeout])
-        embed = discord.Embed(color=0x1ABC9C)
-        embed.set_author(name=message.guild.name, icon_url=message.guild.icon_url)
-        for item in out_list:
-            embed.add_field(name=str(item[0]), value='```python\n' + str(item[1]) + '\n```')
-        await message.channel.send(None, embed=embed)
+    gld = message.guild
+    own = gld.owner
+    response = discord.Embed(color=0x1B6F5F)
+    response.set_author(name=gld.name, icon_url=gld.icon_url)
+    creation_time = arrow.get(gld.created_at).format('DD. MMMM YYYY')
+    bot_count = 0
+    user_count = 0
+    for user in gld.members:
+        if user.bot:
+            bot_count += 1
+        else:
+            user_count += 1
+    guild_text = f'Name: **{gld.name}**'
+    guild_text += f'\nID: **{gld.id}**'
+    guild_text += f'\nMembers: **{user_count}**'
+    guild_text += f'\nBots: **{bot_count}**'
+    guild_text += f'\nChannels: **{len(gld.channels)}**'
+    guild_text += f'\nRoles: **{len(gld.roles)}**'
+    guild_text += f'\nCreated: **{creation_time}**'
+    response.add_field(name='Guild Info', value=guild_text)
+    own_creation_time = arrow.get(own.created_at).format('DD. MMMM YYYY')
+    own_text = f'Username: **{own.name}**#{own.discriminator}'
+    own_text += f'\nNickname: **{own.display_name}**'
+    own_text += f'\nID: **{own.id}**'
+    own_text += f'\nStatus: **{str(own.status).replace("dnd", "busy").title()}**'
+    own_text += f'\nColor: **{str(own.color).upper()}**'
+    own_text += f'\nTop Role: **{own.top_role.name}**'
+    own_text += f'\nCreated: **{own_creation_time}**'
+    response.add_field(name='Owner Info', value=own_text)
+    detail_text = f'AFK Channel: **{gld.afk_channel.name}**'
+    detail_text += f'\nAFK Timeout: **{gld.afk_timeout}**'
+    detail_text += f'\nEmojis: **{len(gld.emojis)}**'
+    detail_text += f'\nLarge: **{gld.large}**'
+    detail_text += f'\nRegion: **{gld.region.name.upper()}**'
+    detail_text += f'\nShard: **{gld.shard_id}**'
+    detail_text += f'\nVerification: **{gld.verification_level.name.upper()}**'
+    response.add_field(name='Details', value=detail_text)
+    await message.channel.send(None, embed=response)
