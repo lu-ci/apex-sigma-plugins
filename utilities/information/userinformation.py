4cc71452c448486a2a689be43e0efad2cba64a27
AXAz0r <thealeksaradovic@gmail.com>
2017-08-09 07:53:22 +0200

    UINFO Command Minify

--- utilities/information/userinformation.py
+++ utilities/information/userinformation.py
@@ -1,27 +1,30 @@
+import arrow
 import discord
 from sigma.core.utilities.data_processing import user_avatar
 
 
 async def userinformation(cmd, message, args):
-    out_list = []
-    if args:
-        user_q = message.mentions[0]
+    if message.mentions:
+        target = message.mentions[0]
     else:
-        user_q = message.author
-    out_list.append(['Username', user_q.name + '#' + user_q.discriminator])
-    out_list.append(['User ID', user_q.id])
-    if user_q.nick:
-        out_list.append(['Nickname', user_q.nick])
-    out_list.append(['Created', str(user_q.created_at)])
-    out_list.append(['Status', str(user_q.status).replace('dnd', 'do not disturb').title()])
-    if user_q.game:
-        out_list.append(['Playing', str(user_q.game).title()])
-    if user_q.roles is not None:
-        out_list.append(['Top Role', str(user_q.top_role)])
-    out_list.append(['Color', str(user_q.color).upper()])
-    out_list.append(['Is Bot', user_q.bot])
-    embed = discord.Embed(color=user_q.color)
-    embed.set_author(name=f'{user_q.name}#{user_q.discriminator}', icon_url=user_avatar(user_q))
-    for item in out_list:
-        embed.add_field(name=str(item[0]), value='```python\n' + str(item[1]) + '\n```')
-    await message.channel.send(None, embed=embed)
+        target = message.author
+    response = discord.Embed(color=target.color)
+    response.set_author(name=f'{target.display_name}\'s Information', icon_url=user_avatar(target))
+    creation_time = arrow.get(target.created_at).format('DD. MMMM YYYY')
+    user_text = f'Username: **{target.name}**#{target.discriminator}'
+    user_text += f'\nID: **{target.id}**'
+    user_text += f'\nStatus: **{str(target.status).replace("dnd", "busy").title()}**'
+    user_text += f'\nBot User: **{target.bot}**'
+    user_text += f'\nCreated: **{creation_time}**'
+    response.add_field(name='User Info', value=user_text, inline=True)
+    member_join_time = arrow.get(target.joined_at).format('DD. MMMM YYYY')
+    is_moderator = target.permissions_in(message.channel).manage_guild
+    member_text = f'Name: **{target.display_name}**'
+    member_text += f'\nColor: **{str(target.color).upper()}**'
+    member_text += f'\nTop Role: **{target.top_role.name}**'
+    member_text += f'\nModerator: **{is_moderator}**'
+    member_text += f'\nJoined: **{member_join_time}**'
+    response.add_field(name='Member Info', value=member_text, inline=True)
+    footer = f'To see the user\'s avatar use the {cmd.bot.get_prefix(message)}avatar command.'
+    response.set_footer(text=footer)
+    await message.channel.send(None, embed=response)
