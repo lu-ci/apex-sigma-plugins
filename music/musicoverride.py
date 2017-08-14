752e4bd35f5f1ced1b8022a0dce1c21bad236ce3
AXAz0r <thealeksaradovic@gmail.com>
2017-08-14 11:26:14 +0200

    Music Override Safeguards Added

--- music/musicoverride.py
+++ music/musicoverride.py
@@ -1,7 +1,21 @@
 async def musicoverride(cmd, message, args):
+    if message.guild.me.voice:
+        try:
+            await message.guild.me.voice.channel.connect()
+        except Exception:
+            pass
     if message.guild.voice_client:
-        await message.guild.voice_client.disconnect()
+        try:
+            await message.guild.voice_client.disconnect()
+        except Exception:
+            pass
     if message.author.voice:
-        await message.author.voice.channel.connect()
+        try:
+            await message.author.voice.channel.connect()
+        except Exception:
+            pass
     if message.guild.voice_client:
-        await message.guild.voice_client.disconnect()
+        try:
+            await message.guild.voice_client.disconnect()
+        except Exception:
+            pass
