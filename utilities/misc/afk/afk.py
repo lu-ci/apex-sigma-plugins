660e5fec1322dfe2f1dc4e970fd5e95046672d5f
AXAz0r <thealeksaradovic@gmail.com>
2017-08-26 14:45:00 +0200

    AFK Sets Image If Image URL Present

--- utilities/misc/afk/afk.py
+++ utilities/misc/afk/afk.py
@@ -17,6 +17,18 @@ async def afk(cmd, message, args):
             'Reason': afk_reason
         }
         cmd.db[cmd.db.db_cfg.database]['AwayUsers'].insert_one(in_data)
-        response = discord.Embed(color=0x66CC66)
-        response.add_field(name='✅ You have been marked as afk.', value=f'Reason: **{afk_reason}**')
+        if afk_reason.startswith('http'):
+            suffix = afk_reason.split('.')[-1]
+            if suffix in ['gif', 'jpg', 'jpeg', 'png']:
+                url = True
+            else:
+                url = False
+        else:
+            url = False
+        if url:
+            response = discord.Embed(color=0x66CC66, title='✅ You have been marked as afk.')
+            response.set_image(url=afk_reason)
+        else:
+            response = discord.Embed(color=0x66CC66)
+            response.add_field(name='✅ You have been marked as afk.', value=f'Reason: **{afk_reason}**')
     await message.channel.send(embed=response)
--- utilities/misc/afk/afk_mention_check.py
+++ utilities/misc/afk/afk_mention_check.py
@@ -1,6 +1,7 @@
 ﻿import arrow
 import discord
 
+
 async def afk_mention_check(ev, message):
     if message.guild:
         if not message.content.startswith(ev.bot.get_prefix(message)):
@@ -10,7 +11,21 @@ async def afk_mention_check(ev, message):
                 if afk_data:
                     time_then = arrow.get(afk_data['Timestamp'])
                     afk_time = arrow.get(time_then).humanize(arrow.utcnow()).title()
-                    response = discord.Embed(color=0x3B88C3, timestamp=time_then.datetime)
-                    response.add_field(name=f'ℹ {target.name} is AFK.',
-                                       value=f'Reason: {afk_data["Reason"]}\nWent AFK: {afk_time}')
+                    afk_reason = afk_data['reason']
+                    if afk_reason.startswith('http'):
+                        suffix = afk_reason.split('.')[-1]
+                        if suffix in ['gif', 'jpg', 'jpeg', 'png']:
+                            url = True
+                        else:
+                            url = False
+                    else:
+                        url = False
+                    if url:
+                        response = discord.Embed(color=0x3B88C3, title=f'ℹ {target.name} is AFK.',
+                                                 timestamp=time_then.datetime)
+                        response.set_thumbnail(url=afk_reason)
+                    else:
+                        response = discord.Embed(color=0x3B88C3, timestamp=time_then.datetime)
+                        response.add_field(name=f'ℹ {target.name} is AFK.',
+                                           value=f'Reason: {afk_data["Reason"]}\nWent AFK: {afk_time}')
                     await message.channel.send(embed=response)
