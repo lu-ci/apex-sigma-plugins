d3959694d9a1ce1edab47bce38633809fd83ee71
AXAz0r <thealeksaradovic@gmail.com>
2017-08-26 14:31:06 +0200

    Test command reverted

--- development/test.py
+++ development/test.py
@@ -2,21 +2,4 @@ import secrets
 
 
 async def test(cmd, message, args):
-    guild_settings = cmd.db.aurora.ServerSettings.find({})
-    for gs in guild_settings:
-        print(f'Checking guild {gs["ServerID"]}')
-        try:
-            if 'WarnedUsers' in gs:
-                warn_dict = gs['WarnedUsers']
-                for uid in warn_dict:
-                    warnings = warn_dict[uid]
-                    new_warns = []
-                    for warning in warnings:
-                        if 'id' not in warning:
-                            warning.update({'id': secrets.token_hex(2)})
-                        new_warns.append(warning)
-                    warn_dict.update({uid: new_warns})
-                cmd.db.set_guild_settings(gs['ServerID'], 'WarnedUsers', warn_dict)
-        except AttributeError:
-            cmd.db.set_guild_settings(gs['ServerID'], 'WarnedUsers', {})
-    await message.channel.send('All done!')
+    await message.channel.send('All good!')
