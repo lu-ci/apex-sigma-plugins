ea4a745491f8a7b5beaa67e68dd8d7b65f39084d
AXAz0r <thealeksaradovic@gmail.com>
2017-08-30 18:29:53 +0200

    Custom Command Command Name

--- moderation/server_settings/custom_commands/custom_command.py
+++ moderation/server_settings/custom_commands/custom_command.py
@@ -2,14 +2,14 @@ from sigma.core.mechanics.permissions import GlobalCommandPermissions
 from sigma.core.utilities.data_processing import command_message_parser
 
 
-def log_command_usage(log, message):
+def log_command_usage(log, message, command):
     if message.guild:
         cmd_location = f'SRV: {message.guild.name} [{message.guild.id}] | '
         cmd_location += f'CHN: #{message.channel.name} [{message.channel.id}]'
     else:
         cmd_location = 'DIRECT MESSAGE'
     author_full = f'{message.author.name}#{message.author.discriminator} [{message.author.id}]'
-    log_text = f'USR: {author_full} | {cmd_location}'
+    log_text = f'USR: {author_full} | {cmd_location} | CMD: {command}'
     log.info(log_text)
 
 
@@ -25,7 +25,7 @@ async def custom_command(ev, message):
                     if custom_commands is None:
                         custom_commands = {}
                     if cmd in custom_commands:
-                        log_command_usage(ev.log, message)
                         cmd_text = custom_commands[cmd]
                         response = command_message_parser(message, cmd_text)
+                        log_command_usage(ev.log, message, cmd)
                         await message.channel.send(response)
