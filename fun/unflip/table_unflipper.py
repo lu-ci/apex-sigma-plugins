9b98113b6896fac10c39e3b30869cc2c0ec76359
AXAz0r <thealeksaradovic@gmail.com>
2017-08-30 17:01:05 +0200

    Changes to DM Detection and Unflip

--- fun/unflip/table_unflipper.py
+++ fun/unflip/table_unflipper.py
@@ -3,13 +3,14 @@ from sigma.core.utilities.stats_processing import add_special_stats
 
 async def table_unflipper(ev, message):
     if '(╯°□°）╯︵ ┻━┻' in message.content:
-        unflip = True
         if message.guild:
             flip_settings = ev.db.get_guild_settings(message.guild.id, 'Unflip')
             if flip_settings is None:
                 unflip = False
             else:
                 unflip = flip_settings
+        else:
+            unflip = True
         if unflip:
             await add_special_stats(ev.db, 'tables_fixed')
             table = ['┬─┬ ノ( ^_^ノ)',
@@ -23,4 +24,5 @@ async def table_unflipper(ev, message):
                      '(ヘ･_･)ヘ┳━┳',
                      'ヘ(´° □°)ヘ┳━┳',
                      '┣ﾍ(≧∇≦ﾍ)… (≧∇≦)/┳━┳']
-            await message.channel.send(secrets.choice(table))
+            table_resp = secrets.choice(table)
+            await message.channel.send(table_resp)
--- help/dm_detection.py
+++ help/dm_detection.py
@@ -1,23 +1,12 @@
-﻿import discord
-
-
-def log_dm(ev, message):
+﻿def log_dm(ev, message):
     author_text = f'{message.author.name}#{message.author.discriminator} [{message.author.id}]'
     ev.log.info(f'DM From {author_text}: {message.content}')
 
 
 async def dm_detection(ev, message):
     if not message.guild:
-        pfx = ev.bot.get_prefix(message)
-        if not message.content.startswith(pfx):
-            log_dm(ev, message)
-            pm_response = discord.Embed(color=0x3B88C3, title=f'ℹ Type `{pfx}help` for information!')
-            await message.channel.send(None, embed=pm_response)
-        else:
-            cmd_name = message.content.split(' ')[0][len(pfx):].lower()
-            if cmd_name in ev.bot.modules.alts:
-                cmd_name = ev.bot.modules.alts[cmd_name]
-            if cmd_name not in ev.bot.modules.commands:
+        if not message.author.bot:
+            pfx = ev.bot.get_prefix(message)
+            if not message.content.startswith(pfx):
                 log_dm(ev, message)
-                pm_response = discord.Embed(color=0x696969, title='🔍 Not A Recognized Command')
-                await message.channel.send(None, embed=pm_response)
+                await ev.bot.modules.commands['help'].execute(message, [])
