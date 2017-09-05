1d5be9de276b35d539221b096689d3185212518e
AXAz0r <thealeksaradovic@gmail.com>
2017-09-05 23:43:36 +0200

    Reload Log Lines

--- owner_controls/core/reload.py
+++ owner_controls/core/reload.py
@@ -2,6 +2,8 @@
 
 
 async def reload(cmd, message, args):
+    cmd.log.info('---------------------------------')
+    cmd.log.info('Reloading all modules...')
     cmd.bot.ready = False
     response = discord.Embed(color=0xF9F9F9, title='⚗ Reloading all modules...')
     load_status = await message.channel.send(embed=response)
@@ -14,4 +16,6 @@ async def reload(cmd, message, args):
     load_done_resonse = discord.Embed(color=0x77B255, title=f'✅ Loaded {cmd_count} Commands and {ev_count} Events.')
     await load_status.edit(embed=load_done_resonse)
     cmd.bot.ready = True
+    cmd.log.info(f'Loaded {cmd_count} commands and {ev_count} events.')
+    cmd.log.info('---------------------------------')
 
