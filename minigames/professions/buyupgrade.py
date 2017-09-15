752a84b6a00c4497aa30f24a3ff005b0444ea446
AXAz0r <thealeksaradovic@gmail.com>
2017-09-15 14:00:10 +0200

    Price Scaling Increase

--- minigames/professions/buyupgrade.py
+++ minigames/professions/buyupgrade.py
@@ -41,7 +41,7 @@ async def buyupgrade(cmd, message, args):
         if upgrade_level == 0:
             upgrade_price = base_price
         else:
-            price_mod = int(base_price * upgrade_level * 1.5)
+            price_mod = int(base_price * upgrade_level * 1.75)
             upgrade_price = price_mod + (price_mod // 2)
         currency = cmd.bot.cfg.pref.currency
         next_upgrade = upgrade_level + 1
