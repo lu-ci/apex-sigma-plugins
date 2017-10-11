3ae6cf35cda13459c73562e528352529d599c1f5
AXAz0r <thealeksaradovic@gmail.com>
2017-10-11 14:39:27 +0200

    Avatar Command Arguments

--- utilities/images/avatar.py
+++ utilities/images/avatar.py
@@ -1,13 +1,27 @@
 import discord
 from sigma.core.utilities.data_processing import user_avatar
+from sigma.core.utilities.data_processing import get_image_colors
 
 
 async def avatar(cmd, message, args):
+    gif = False
+    auto_color = False
+    if args:
+        if args[-1].lower() == 'gif':
+            gif = True
+        elif args[-1].lower() == 'auto':
+            auto_color = True
     if message.mentions:
         target = message.mentions[0]
     else:
         target = message.author
-    ava_url = user_avatar(target)
-    embed = discord.Embed(color=target.color)
+    ava_url = user_avatar(target, gif)
+    if auto_color:
+        color = await get_image_colors(ava_url)
+    else:
+        color = target.color
+    embed = discord.Embed(color=color)
+    if auto_color:
+        embed.description = f'Dominant Color: #{str(hex(color)).split("x")[1].upper()}'
     embed.set_image(url=ava_url)
     await message.channel.send(None, embed=embed)
