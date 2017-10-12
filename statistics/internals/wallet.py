56da77c785adebe9252619d3ef79a67763723ddb
AXAz0r <thealeksaradovic@gmail.com>
2017-10-12 14:37:19 +0200

    Kud Command Footer Fix

--- statistics/internals/wallet.py
+++ statistics/internals/wallet.py
@@ -19,5 +19,5 @@ async def wallet(cmd, message, args):
     response.add_field(name=current_title, value=f"```py\n{currency['current']} {currency_name}\n```", inline=True)
     response.add_field(name=guild_title, value=f"```py\n{currency['guild']} {currency_name}\n```", inline=True)
     response.add_field(name=global_title, value=f"```py\n{currency['global']} {currency_name}\n```", inline=True)
-    response.set_footer(text=f'{currency_icon} {currency_name} is earned by being an active member of the community.')
+    response.set_footer(text=f'{currency_icon} {currency_name} is earned by participating in minigames.')
     await message.channel.send(embed=response)
