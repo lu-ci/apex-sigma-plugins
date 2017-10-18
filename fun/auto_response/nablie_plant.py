07882f4270f4764b97030a9665c8776d32eb45fd
AXAz0r <thealeksaradovic@gmail.com>
2017-10-18 20:24:28 +0200

    Added the Nab Responder

---
+++ fun/auto_response/nablie_plant.py
@@ -0,0 +1,12 @@
+import secrets
+import discord
+
+
+async def nablie_plant(ev, message):
+    if message.author.id == 211922001546182656:
+        roll = secrets.randbelow(20)
+        if roll == 0:
+            try:
+                await message.add_reaction('🍆')
+            except discord.Forbidden:
+                pass
