30a7cc12b94b570c374e22955fbfbcdf8562abbb
AXAz0r <thealeksaradovic@gmail.com>
2017-10-18 19:50:07 +0200

    Quote Safeguards

--- fun/quotes/quote.py
+++ fun/quotes/quote.py
@@ -9,22 +9,27 @@ async def quote(cmd, message, args):
     tries = 0
     while not data and tries < 5:
         async with aiohttp.ClientSession() as session:
-            async with session.get(resource) as data:
-                data = await data.read()
+            async with session.get(resource) as page_data:
                 try:
-                    data = json.loads(data)
+                    byte_data = await page_data.read()
+                    data = json.loads(byte_data)
                 except json.JSONDecodeError:
                     tries += 1
-    text = data['quoteText']
-    while text.endswith(' '):
-        text = text[:-1]
-    if 'quoteAuthor' in data:
-        author = data['quoteAuthor']
+    if data:
+        text = data['quoteText']
+        while text.endswith(' '):
+            text = text[:-1]
+        if 'quoteAuthor' in data:
+            author = data['quoteAuthor']
+        else:
+            author = 'Unknown'
+        if not author:
+            author = 'Unknown'
+        quote_text = f'\"{text}\"'
     else:
-        author = 'Unknown'
-    if not author:
-        author = 'Unknown'
-    quote_text = f'\"{text}\"\n'
-    embed = discord.Embed(color=0xF9F9F9)
-    embed.add_field(name=f'📑 A Quote From {author}', value=quote_text)
-    await message.channel.send(None, embed=embed)
+        author = 'Sir Winston Churchill'
+        quote_text = '"Some people\'s " idea of free speech is that they are free to say'
+        quote_text += f' what the like, but if anyone says anything back, that is an outrage."'
+    response = discord.Embed(color=0xF9F9F9)
+    response.add_field(name=f'📑 A Quote From {author}', value=quote_text)
+    await message.channel.send(None, embed=response)
