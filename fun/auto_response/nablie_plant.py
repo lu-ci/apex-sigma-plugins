823824282a321d67299baaeed9610a46157d04e8
AXAz0r <thealeksaradovic@gmail.com>
2017-10-24 19:58:13 +0200

    Nab Response Chances Increased

--- fun/auto_response/nablie_plant.py
+++ fun/auto_response/nablie_plant.py
@@ -4,7 +4,7 @@ import discord
 
 async def nablie_plant(ev, message):
     if message.author.id == 211922001546182656:
-        roll = secrets.randbelow(20)
+        roll = secrets.randbelow(5)
         if roll == 0:
             try:
                 await message.add_reaction('🍆')
