9238afa1d6843e5bd701fa42e986f1f2865bb878
AXAz0r <thealeksaradovic@gmail.com>
2017-10-27 05:16:03 +0200

    ToggleRole Position Check

--- moderation/server_settings/roles/self_assignable_roles/togglerole.py
+++ moderation/server_settings/roles/self_assignable_roles/togglerole.py
@@ -7,18 +7,24 @@ async def togglerole(cmd, message, args):
         lookup = ' '.join(args)
         target_role = matching_role(message.guild, lookup)
         if target_role:
-            selfroles = cmd.db.get_guild_settings(message.guild.id, 'SelfRoles')
-            if selfroles is None:
-                selfroles = []
-            if target_role.id in selfroles:
-                user_role_match = user_matching_role(message.author, target_role.name)
-                if not user_role_match:
-                    await message.author.add_roles(target_role, reason='Role self assigned.')
-                    response = discord.Embed(color=0x77B255, title=f'✅ {target_role.name} has been **added** to you.')
+            self_roles = cmd.db.get_guild_settings(message.guild.id, 'SelfRoles')
+            if self_roles is None:
+                self_roles = []
+            if target_role.id in self_roles:
+                role_bellow = bool(target_role.position < message.guild.me.top_role.position)
+                if role_bellow:
+                    user_role_match = user_matching_role(message.author, target_role.name)
+                    if not user_role_match:
+                        await message.author.add_roles(target_role, reason='Role self assigned.')
+                        addition_title = f'✅ {target_role.name} has been **added** to you.'
+                        response = discord.Embed(color=0x77B255, title=addition_title)
+                    else:
+                        await message.author.remove_roles(target_role, reason='Role self assigned.')
+                        removal_title = f'💣 {target_role.name} has been **removed** from you.'
+                        response = discord.Embed(color=0x262626, title=removal_title)
                 else:
-                    await message.author.remove_roles(target_role, reason='Role self assigned.')
-                    response = discord.Embed(color=0x77B255,
-                                             title=f'✅ {target_role.name} has been **removed** from you.')
+                    role_hierarchy_error = '❗ This role is above my highest role. I can not manage it.'
+                    response = discord.Embed(color=0xBE1931, title=role_hierarchy_error)
             else:
                 response = discord.Embed(color=0xFFCC4D, title=f'⚠ {target_role} is not self assignable.')
         else:
