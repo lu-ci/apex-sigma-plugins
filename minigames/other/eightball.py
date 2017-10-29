8e7b032c9a46f4cb79dbc0e5ebbc35b4efe2f170
AXAz0r <thealeksaradovic@gmail.com>
2017-10-29 17:22:57 +0100

    Changed 8Ball Mechanics

--- minigames/other/eightball.py
+++ minigames/other/eightball.py
@@ -1,7 +1,23 @@
 ﻿import secrets
 import discord
 
-answers = [
+positive = [
+    'Absolutely.',
+    'I am certain that that\'s a yes.',
+    'Of course.',
+    'Yes.',
+    'No shit Sherlock.',
+    'Is water wet?',
+    'Maybe, probably, certainly, yeah.',
+    'Yup.',
+    'Yeah.',
+    'For sure.',
+    'Undoubtedly.',
+    '100% Certainty.',
+    'This is an absolute.',
+    'True enough.',
+    'True.',
+    'Senpai says yes.',
     'It is certain.',
     'It is decidedly so.',
     'Without a doubt.',
@@ -9,14 +25,37 @@ answers = [
     'You may rely on it.',
     'I find it highly plausible.',
     'Most likely.',
-    'Outlook good.',
-    'Yes.',
-    'Signs point to yes.',
-    'I am a bit hazy, try again later.',
-    'I\'m on my coffee break, ask later.',
-    'Now is not the time.',
-    'I am drawing a blank.',
-    'You lack determination, I got nothing.',
+    'Outlook good.'
+]
+
+neural = [
+    'I\'m not sure.',
+    'Ask me later.',
+    'I can\'t say for certain.',
+    'I don\'t have enough data.',
+    'I\'m on a break, ask again later.',
+    'Too tired, ask me after a nap.',
+    'Too lazy to calculate now.',
+    'Maybe, I\'m not really sure.',
+    'Ughhhh... Not sure...',
+    'Lunch time, piss off and ask me later.',
+    'Senpai failed to notice your question.',
+    'Is that even a valid question?',
+    'I am drawing a blank.'
+]
+
+negative = [
+    'No.',
+    'Nope.',
+    'Nu-uh.',
+    'Negative.',
+    'False.',
+    'That would be a no.',
+    'Sorry, that\'s a negative.',
+    'Nothing like that.',
+    'Not how it goes.',
+    'Nah bro, ain\'t nothing like that.',
+    'Senpai says no.',
     'Don\'t count on it.',
     'My reply is no.',
     'My sources say no.',
@@ -27,6 +66,13 @@ answers = [
 
 async def eightball(cmd, message, args):
     if args:
+        roll = secrets.randbelow(4)
+        if roll == 0:
+            answers = negative
+        elif roll == 1:
+            answers = neural
+        else:
+            answers = positive
         answer = secrets.choice(answers)
         response = discord.Embed(color=0x232323, title=f'🎱 {answer}')
     else:
