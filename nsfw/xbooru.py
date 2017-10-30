c8bd2ce44ad2476d0da7922ad4d4b1369e29cf26
AXAz0r <thealeksaradovic@gmail.com>
2017-10-30 12:50:57 +0100

    Fix Empty Search Response

--- nsfw/xbooru.py
+++ nsfw/xbooru.py
@@ -44,5 +44,5 @@ async def xbooru(cmd, message, args):
         response.set_image(url=img_url)
         response.set_footer(text=footer_text)
     else:
-        response = discord.Embed(color=0x696969, title=f'🔍 Search for yielded no results.')
+        response = discord.Embed(color=0x696969, title=f'🔍 Search yielded no results.')
     await message.channel.send(None, embed=response)
