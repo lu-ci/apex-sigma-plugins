72657bf7552baf848c98375e6b2dbbe3bc5f8118
AXAz0r <thealeksaradovic@gmail.com>
2017-11-04 00:42:43 +0100

    Just Monika On Multiple Servers

--- fun/auto_response/just_monika.py
+++ fun/auto_response/just_monika.py
@@ -1,7 +1,10 @@
+active_guilds = [138067606119645184, 200751504175398912, 198184144986046470]
+
+
 async def just_monika(ev, message):
     if message.content:
         if message.guild:
-            if message.guild.id == 138067606119645184:
+            if message.guild.id in active_guilds:
                 args = message.content.split()
                 triggered = False
                 for arg in args:
