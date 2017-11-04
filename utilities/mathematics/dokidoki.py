0e9609493ed8c1cbcd0074f3b16036e3f02b7575
AXAz0r <thealeksaradovic@gmail.com>
2017-11-04 18:16:00 +0100

    DD Glitch Addition

--- utilities/mathematics/dokidoki.py
+++ utilities/mathematics/dokidoki.py
@@ -11,6 +11,10 @@ titles = {
     'm': 'Cacophony of colors...'
 }
 
+titles_glitch = {
+    'm': 'Flash ng, exp nd ng, piercing...'
+}
+
 chars = {
     'n': [
         'https://i.imgur.com/Xr19wxI.png',
@@ -28,6 +32,10 @@ chars = {
         'https://i.imgur.com/wJazxfj.png',
         'https://i.imgur.com/6qqfqC7.png'
     ]
+}
+
+chars_glitch = {
+    'm': 'https://i.imgur.com/im1H8jA.png'
 }
 
 
@@ -51,12 +59,29 @@ async def dokidoki(cmd, message, args):
         except InvalidToken:
             ciphered = None
         if ciphered:
-            output = markovify.Text(ciphered).make_short_sentence(750)
-            output = clean(output, message.author)
-            title = titles['m']
+            glitch = secrets.randbelow(10)
+            glitch = not bool(glitch)
+            if glitch:
+                line_count = 1
+                thumbnail = chars_glitch['m']
+            else:
+                line_count = 3
+                thumbnail = secrets.choice(chars['m'])
+            lines = []
+            for x in range(0, line_count):
+                output = markovify.Text(ciphered).make_short_sentence(500, tries=100)
+                output = clean(output, message.author)
+                if glitch:
+                    output = cipher.encrypt(output.encode('utf-8')).decode('utf-8')
+                lines.append(output)
+            output_final = ' '.join(lines)
+            if glitch:
+                title = titles_glitch['m']
+            else:
+                title = titles['m']
             response = discord.Embed(color=0xe75a70)
-            response.add_field(name=f'💟 {title}', value=output)
-            response.set_thumbnail(url=secrets.choice(chars['m']))
+            response.add_field(name=f'💟 {title}', value=output_final)
+            response.set_thumbnail(url=thumbnail)
         else:
             response = discord.Embed(color=0xe75a70, title='💔 Sorry but that key is incorrect!')
     else:
