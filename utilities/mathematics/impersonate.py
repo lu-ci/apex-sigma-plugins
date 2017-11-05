818e3e699a63d0827f0c2ceea11c8be71f7f6912
AXAz0r <thealeksaradovic@gmail.com>
2017-11-05 09:19:56 +0100

    Impersonate Performance Improvements

--- utilities/mathematics/impersonate.py
+++ utilities/mathematics/impersonate.py
@@ -2,7 +2,6 @@
 from concurrent.futures import ThreadPoolExecutor
 
 import discord
-import ftfy
 import markovify
 
 from sigma.core.utilities.data_processing import user_avatar
@@ -26,15 +25,13 @@ async def impersonate(cmd, message, args):
             chain_data = cmd.db[cmd.db.db_cfg.database]['MarkovChains'].find_one({'UserID': target.id})
             if chain_data:
                 total_string = ' '.join(chain_data['Chain'])
-                total_string = ftfy.fix_text(total_string)
                 chain = await cmd.bot.loop.run_in_executor(threads, functools.partial(markovify.Text, total_string))
-                chain_function = functools.partial(chain.make_short_sentence, max_chars=150)
+                chain_function = functools.partial(chain.make_short_sentence, max_chars=500)
                 task = cmd.bot.loop.run_in_executor(threads, chain_function)
                 sentence = await task
                 if not sentence:
                     response = discord.Embed(color=0xBE1931, title='😖 I could not think of anything...')
                 else:
-                    sentence = ftfy.fix_text(sentence)
                     response = discord.Embed(color=0xbdddf4)
                     response.set_author(name=target.name, icon_url=user_avatar(target))
                     response.add_field(name='💭 Hmm... something like...', value=sentence)
