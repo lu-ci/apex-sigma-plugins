0f7f0c4daf34319e584e985636d6d0578ede1532
AXAz0r <thealeksaradovic@gmail.com>
2017-11-06 12:09:45 +0100

    Added RibbleCurry Response

--- fun/auto_response/nablie_plant.py
+++ fun/auto_response/nablie_plant.py
@@ -2,12 +2,17 @@ import secrets
 
 import discord
 
+responses = {
+    150060705662500864: '🍛',
+    211922001546182656: '🍆'
+}
+
 
 async def nablie_plant(ev, message):
-    if message.author.id == 211922001546182656:
+    if message.author.id in responses:
         roll = secrets.randbelow(5)
         if roll == 0:
             try:
-                await message.add_reaction('🍆')
+                await message.add_reaction(responses.get(message.author.id))
             except discord.Forbidden:
                 pass
